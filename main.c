c8e072f46dce19fba48e04233d1633a73022d9d4
Juan <49064426+JuanPMC@users.noreply.github.com>
2020-11-30 20:46:09 +0100

    1.2 Refactorizado funcion de lectra de fichero

--- main.c
+++ main.c
@@ -3,20 +3,25 @@
 #define TAMANIO_LINEA 8
 #define CANTIDAD_LINEAS 4
 #define TAMANIO_RAM 1024
+
 //ETQ=5bits,linea=2bits,palabra=3bits
 typedef struct {    // una linea de chache
     short int ETQ;
     short int Datos[TAMANIO_LINEA];
 } T_LINEA_CACHE;
+
 T_LINEA_CACHE cahce[CANTIDAD_LINEAS];
 unsigned char RAM[TAMANIO_RAM];
+
 int tiempoglobal, numerofallos;
+
 void cargarRamEnRam(){
     FILE *ramBinary;
     ramBinary = fopen("RAM.bin","rb");  // r para leer y b de binario
     fread(RAM,TAMANIO_RAM,1,ramBinary); // leer el fichero y cargarlo en la ram
     fclose(ramBinary);
 }
+
 int decimal_a_binario(int dec){//Con esta funcion nos va a salir el binario empezando por el primer 1 de mayor orden
     int rem, c = 1;
     long int bin = 0;
@@ -28,6 +33,36 @@ int decimal_a_binario(int dec){//Con esta funcion nos va a salir el binario empe
     }
     return bin;
 }
+
+long int* peticiones_de_lectura(){
+    FILE *accesomem;
+    char linea[6];
+    static long int resultado[100];
+    int memoria;
+    int i;
+
+    // abrir fichero
+    accesomem = fopen("accesos_memoria.txt","rt");
+
+    // gestion de fallos
+    if (accesomem == NULL){
+        return NULL;
+        printf("fallo al abrir el fichero");
+    }
+
+    // operar con fichero
+    for(i = 0; fgets(linea, 6,accesomem); i++){
+        memoria=strtol(linea,NULL,16); // pasar a base 10 y a entero
+        resultado[i] = decimal_a_binario(memoria);  // guardar en array de accesos
+    }
+    resultado[i] = -1; // senializar el final del buffer
+
+    // cerrar fichero
+    fclose(accesomem);
+
+    return resultado; // retornar el array
+}
+
 void arranque(){
     // inicializamos la cache
     for(int i = 0; i < CANTIDAD_LINEAS; i++){
@@ -36,35 +71,16 @@ void arranque(){
             cahce[i].Datos[j]=0;
     }
     cargarRamEnRam();
-    FILE *accesomem;
-    char linea[12];
-    char *resultado;
-    int memoria;
-    char etiqueta;
-    char *ptr;
-    accesomem = fopen("accesos_memoria.txt","rt");
-    if (accesomem == NULL){
-        //exit(-1)
-        printf("fallo al abrir el fichero");
-    }
-    else{
-        int a;
-        while(a!=13){
-            for(int b=0;b<=12;b++){
-                printf("Hex:%s",linea);//Los tres printf de este for son orientativos para ver si se esta haciendo bien
-                memoria=strtol(linea,&ptr,16);
-                printf("Dec y entero:%d\n",memoria);
-                printf("Etiqueta:%d\n",decimal_a_binario(memoria));
-                fgets(linea, 12,accesomem);
-                a++;
-            }    
-        }
-        fclose(accesomem);
-    }
 }
+
 int main(){
+    long int *accesosAmemoria;
     tiempoglobal = numerofallos = 0;
+
     arranque();
-    printf("Test RAM: %d", RAM[1]);
+    accesosAmemoria = peticiones_de_lectura();
+
+    printf("Test RAM: %d, Test Lectura: %d", RAM[1], accesosAmemoria[1]);
+
     return 0;
 }
